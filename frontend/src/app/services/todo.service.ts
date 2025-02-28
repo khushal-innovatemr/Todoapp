00e71511ef71ff3939b85b2d5574d63a25f5d75f
Khushal <khushal.innovatemr@gmail.com>
2025-02-28 12:15:36 +0530

    frontend-backend

--- frontend/src/app/services/todo.service.ts
+++ frontend/src/app/services/todo.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class TodoService {
   readonly Root;
   constructor(private http :HttpClient) {
-    this.Root = 'https://todoapp-3oh4.onrender.com'
+    this.Root = 'https://todoapp-3oh4.onrender.com/'
    }
 
   get(uri:string){
